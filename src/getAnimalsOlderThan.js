9d21381d24f2bd7b2d489239c7f1b3cc06b19e3f
danillo <danillogonvalves001@gmail.com>
2022-01-25 17:43:33 -0300

    Requisito 2 feito.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const lookForSpecies = species.find((element) => element.name === animal);
+  console.log(lookForSpecies);
+  return lookForSpecies.residents.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
