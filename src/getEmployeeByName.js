31ec868504294af4cf62e59caae48282740e1063
danillo <danillogonvalves001@gmail.com>
2022-01-25 18:22:57 -0300

    Requisito 3 feito.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees
+    .find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
