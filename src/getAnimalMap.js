7e8fe210a26dff14d78adc272f75fb1a968196a3
danillo <danillogonvalves001@gmail.com>
2022-01-26 01:53:25 -0300

    Requisito 7 em construçao.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,35 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const animalMap = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+};
+
+const notIncludeNamesAnimalMap = () => {
+  species.forEach((element) => animalMap[element.location].push(element.name));
+  return animalMap;
+};
+
+const namesAnimalMap = () => {
+  const speciesName = {};
+  species.forEach((element) => {
+    speciesName[element.name] = []; // Aqui deu ruim
+    element.residents.map((nameAnimal) => speciesName[element.name].push(nameAnimal.name));
+    animalMap[element.location].push(speciesName[element.name]);
+  });
+  return animalMap;
+};
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options || !options.includeNames) {
+    return notIncludeNamesAnimalMap();
+  }
+  if (options.includeNames) {
+    return namesAnimalMap();
+  }
 }
-
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
