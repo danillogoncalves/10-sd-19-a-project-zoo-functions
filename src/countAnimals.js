d863b35193901dedb5ab1edd02a1938a088071c0
danillo <danillogonvalves001@gmail.com>
2022-01-25 22:01:54 -0300

    Requisito 5 feito.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const allAnimal = {};
+  if (!animal) {
+    species.forEach((element) => {
+      allAnimal[element.name] = (element.residents.length);
+    });
+    return allAnimal;
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  const sex = species.find((element) => element.name === animal.specie).residents;
+  return sex.reduce((acc, curr) => acc + (curr.sex === animal.sex ? 1 : 0), 0);
 }
 
 module.exports = countAnimals;
