2c3269d03ddfa290954ec3015754fc8ddabd6962
danillo <danillogonvalves001@gmail.com>
2022-01-27 19:24:00 -0300

    Requisito 10 em construçao.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { employees, species } = data;
+
+const employeesId = employees.map((ids) => `${ids.id}`);
+const employeesFirstName = employees.map((firstsNames) => `${firstsNames.firstName}`);
+const employeesLastName = employees.map((lastsNames) => `${lastsNames.lastName}`);
+
+const whatIsTheKey = (nameOrId) => {
+  if (employeesId.includes(nameOrId.id)) {
+    return 'id';
+  }
+  if (employeesFirstName.includes(nameOrId.name)) {
+    return 'firstName';
+  }
+  if (employeesLastName.includes(nameOrId.name)) {
+    return 'lastName';
+  }
+};
+
+const createEmployeesCoverage = () => {
+  employees.map((employee) => ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    // species: employee.responsibleFor.map((specie) => )
+  }));
+};
+
+function getEmployeesCoverage(nameOrId) {
   // seu código aqui
 }
+
+console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
 
 module.exports = getEmployeesCoverage;
