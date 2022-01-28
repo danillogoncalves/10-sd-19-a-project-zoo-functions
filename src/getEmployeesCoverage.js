deb3577763345f4b81d5d51b0a6b53a9fad75a68
danillo <danillogonvalves001@gmail.com>
2022-01-27 23:30:33 -0300

    Requisito 10 feito.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,21 +2,45 @@ const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
-// const employeesId = employees.map((ids) => `${ids.id}`);
-// const employeesFirstName = employees.map((firstsNames) => `${firstsNames.firstName}`);
-// const employeesLastName = employees.map((lastsNames) => `${lastsNames.lastName}`);
+const employeesIds = employees.map((ids) => `${ids.id}`);
+const employeesFirstsNames = employees.map((firstsNames) => `${firstsNames.firstName}`);
+const employeesLastsNames = employees.map((lastsNames) => `${lastsNames.lastName}`);
 
-// const whatIsTheKey = (nameOrId) => {
-//   if (employeesId.includes(nameOrId.id)) {
-//     return 'id';
-//   }
-//   if (employeesFirstName.includes(nameOrId.name)) {
-//     return 'firstName';
-//   }
-//   if (employeesLastName.includes(nameOrId.name)) {
-//     return 'lastName';
-//   }
-// };
+const createEmployeesCoverageLastName = (employeesLastName) => {
+  const result = [employees.find((employee) => employee.lastName === employeesLastName.name)];
+  return result.map((employeeLastName) => ({
+    id: employeeLastName.id,
+    fullName: `${employeeLastName.firstName} ${employeeLastName.lastName}`,
+    species: employeeLastName.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).name),
+    locations: employeeLastName.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).location),
+  }))[0];
+};
+
+const createEmployeesCoverageFirstName = (employeesFirstName) => {
+  const result = [employees.find((employee) => employee.firstName === employeesFirstName.name)];
+  return result.map((employeeFirstName) => ({
+    id: employeeFirstName.id,
+    fullName: `${employeeFirstName.firstName} ${employeeFirstName.lastName}`,
+    species: employeeFirstName.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).name),
+    locations: employeeFirstName.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).location),
+  }))[0];
+};
+
+const createEmployeesCoverageId = (employeesId) => {
+  const result = [employees.find((employee) => employee.id === employeesId.id)];
+  return result.map((employeeId) => ({
+    id: employeeId.id,
+    fullName: `${employeeId.firstName} ${employeeId.lastName}`,
+    species: employeeId.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).name),
+    locations: employeeId.responsibleFor.map((specie) => species
+      .find((animal) => animal.id === specie).location),
+  }))[0];
+};
 
 const createEmployeesCoverage = () => employees.map((employee) => ({
   id: employee.id,
@@ -29,9 +53,19 @@ const createEmployeesCoverage = () => employees.map((employee) => ({
 
 function getEmployeesCoverage(nameOrId) {
   // seu código aqui
-  return createEmployeesCoverage();
+  if (!nameOrId) {
+    return createEmployeesCoverage();
+  }
+  if (employeesIds.includes(nameOrId.id)) {
+    return createEmployeesCoverageId(nameOrId);
+  }
+  if (employeesFirstsNames.includes(nameOrId.name)) {
+    return createEmployeesCoverageFirstName(nameOrId);
+  }
+  if (employeesLastsNames.includes(nameOrId.name)) {
+    return createEmployeesCoverageLastName(nameOrId);
+  }
+  throw new Error('Informações inválidas');
 }
-
-console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
