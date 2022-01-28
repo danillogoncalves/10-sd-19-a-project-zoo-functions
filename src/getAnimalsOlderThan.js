5599a27c5bbeaf98855b9561b14058606a675e1a
danillo <danillogonvalves001@gmail.com>
2022-01-28 00:14:50 -0300

    Retirando os console.log.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,6 @@ const { species } = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const lookForSpecies = species.find((element) => element.name === animal);
-  console.log(lookForSpecies);
   return lookForSpecies.residents.every((element) => element.age >= age);
 }
 
