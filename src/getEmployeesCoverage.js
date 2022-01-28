1841b7611c8c3fca0fd4a9724dcafd42de8293bf
danillo <danillogonvalves001@gmail.com>
2022-01-27 21:19:36 -0300

    Requisito 10 em construçao.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,34 +2,36 @@ const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
-const employeesId = employees.map((ids) => `${ids.id}`);
-const employeesFirstName = employees.map((firstsNames) => `${firstsNames.firstName}`);
-const employeesLastName = employees.map((lastsNames) => `${lastsNames.lastName}`);
+// const employeesId = employees.map((ids) => `${ids.id}`);
+// const employeesFirstName = employees.map((firstsNames) => `${firstsNames.firstName}`);
+// const employeesLastName = employees.map((lastsNames) => `${lastsNames.lastName}`);
 
-const whatIsTheKey = (nameOrId) => {
-  if (employeesId.includes(nameOrId.id)) {
-    return 'id';
-  }
-  if (employeesFirstName.includes(nameOrId.name)) {
-    return 'firstName';
-  }
-  if (employeesLastName.includes(nameOrId.name)) {
-    return 'lastName';
-  }
-};
+// const whatIsTheKey = (nameOrId) => {
+//   if (employeesId.includes(nameOrId.id)) {
+//     return 'id';
+//   }
+//   if (employeesFirstName.includes(nameOrId.name)) {
+//     return 'firstName';
+//   }
+//   if (employeesLastName.includes(nameOrId.name)) {
+//     return 'lastName';
+//   }
+// };
 
-const createEmployeesCoverage = () => {
-  employees.map((employee) => ({
-    id: employee.id,
-    fullName: `${employee.firstName} ${employee.lastName}`,
-    // species: employee.responsibleFor.map((specie) => )
-  }));
-};
+const createEmployeesCoverage = () => employees.map((employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: employee.responsibleFor.map((specie) => species
+    .find((animal) => animal.id === specie).name),
+  locations: employee.responsibleFor.map((specie) => species
+    .find((animal) => animal.id === specie).location),
+}));
 
 function getEmployeesCoverage(nameOrId) {
   // seu código aqui
+  return createEmployeesCoverage();
 }
 
-console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
